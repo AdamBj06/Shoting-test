bbefb050b824514cf8c4ea2864706518643bf27c
Adam Ben jeddou <mcadambenjeddou@gmail.com>
2024-06-21 18:46:32 +0200

    .

--- Shoting test/Form1.cs
+++ Shoting test/Form1.cs
@@ -13,19 +13,12 @@ namespace Shoting_test
 {
     public partial class Form1 : Form
     {
-        Graphics g;
-        Random rnd = new Random();
-        Player p1 = new Player(Color.Black, 16, new Vettore(300, 300), new Vettore(0, 0));
-        Projectile prj = new Projectile(Color.Red, 6, 6, 40);
-        Projectile bomb = new Projectile(Color.DarkGray, 10, 4, 4);
-        Projectile special = new Projectile(Color.Yellow, 4, 7, 0, 16);
-        Projectile special2 = new Projectile(Color.Yellow, 4, 7, 6, 30);
-        List<Rectangle> fruits = new List<Rectangle>();
         public Form1()
         {
             InitializeComponent();
         }
 
+        Graphics g;
         private void Form1_Shown(object sender, EventArgs e)
         {
             WindowState = FormWindowState.Maximized;
@@ -34,10 +27,16 @@ namespace Shoting_test
             timer2.Enabled = true;
         }
 
+        Player p1 = new Player(Color.Black, 16, new Vettore(300, 300), new Vettore(0, 0), 2);
+        Player p2 = new Player(Color.Green, 16, new Vettore(600, 300), new Vettore(0, 0), 2);
+        Projectile special = new Projectile(Color.Yellow, 4, 7, 0, 16);
+        Projectile special2 = new Projectile(Color.Yellow, 4, 7, 60000, 300000);
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            p1.MoveStart(e);
-            if(e.KeyCode == Keys.Q && special.CurrentEnergy >= 4)
+            p1.MoveStart(e, Keys.W, Keys.S, Keys.A, Keys.D);
+            p2.MoveStart(e, Keys.Up, Keys.Down, Keys.Left, Keys.Right);
+
+            if (e.KeyCode == Keys.Q && special.CurrentEnergy >= 4)
             {
                 special.Shoot(new Vettore(0, 1), p1);
                 special.Shoot(new Vettore(0, -1), p1);
@@ -56,9 +55,12 @@ namespace Shoting_test
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            p1.MoveEnd(e);
+            p1.MoveEnd(e, Keys.W, Keys.S, Keys.A, Keys.D);
+            p2.MoveEnd(e, Keys.Up, Keys.Down, Keys.Left, Keys.Right);
         }
-        
+
+        Projectile prj = new Projectile(Color.Red, 6, 6, 40);
+        Projectile bomb = new Projectile(Color.DarkGray, 10, 4, 4);
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
             if(e.Button == MouseButtons.Left)
@@ -71,6 +73,8 @@ namespace Shoting_test
             }
         }
 
+        List<Rectangle> fruits = new List<Rectangle>();
+        Random rnd = new Random();
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (fruits.Count() <= 0)
@@ -83,6 +87,7 @@ namespace Shoting_test
             }
             
             p1.MovePlayer(g, BackColor);
+            p2.MovePlayer(g, BackColor);
 
             for (int i = 0; i < fruits.Count(); i++)
             {
@@ -112,7 +117,7 @@ namespace Shoting_test
                                         , special2.CurrentEnergy / 3, special2.Energy / 3);
         }
 
-        private void timer2_Tick(object sender, EventArgs e)//refreshes
+        private void timer2_Tick(object sender, EventArgs e)
         {
             for (int i = 0; i < fruits.Count(); i++)
             {
--- Shoting test/Player.cs
+++ Shoting test/Player.cs
@@ -15,39 +15,71 @@ namespace Shoting_test
         public int Size { get; set;}
         public Vettore PosPlayer { get; set; }
         public Vettore Speed { get; set; }
-        public Player(Color color, int size, Vettore initialPos, Vettore intialSpeed) 
+        public int SpeedValue { get; set; }
+        public Player(Color color, int size, Vettore initialPos, Vettore intialSpeed, int speedValue) 
         {
             Color = color;
             Size = size;
             Speed = intialSpeed;
             PosPlayer = initialPos;
+            SpeedValue = speedValue;
         }
-        public void MoveStart(KeyEventArgs e)
+        public void MoveStart(KeyEventArgs e, Keys up, Keys down, Keys left, Keys right)
         {
-            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            if (e.KeyCode == up)
             {
-                Speed.Y = -2;
+                Speed.Y = -SpeedValue;
             }
-            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            if (e.KeyCode == left)
             {
-                Speed.X = -2;
+                Speed.X = -SpeedValue;
             }
-            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            if (e.KeyCode == down)
             {
-                Speed.Y = 2;
+                Speed.Y = SpeedValue;
             }
-            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            if (e.KeyCode == right)
             {
-                Speed.X = 2;
+                Speed.X = SpeedValue;
             }
         }
-        public void MoveEnd(KeyEventArgs e)
+        public void MoveStart(KeyEventArgs e, Keys up, Keys down, Keys left, Keys right, Keys up2, Keys down2, Keys left2, Keys right2)
         {
-            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.W || e.KeyCode == Keys.S)
+            if (e.KeyCode == up || e.KeyCode == up2)
+            {
+                Speed.Y = -SpeedValue;
+            }
+            if (e.KeyCode == left || e.KeyCode == left2)
+            {
+                Speed.X = -SpeedValue;
+            }
+            if (e.KeyCode == down || e.KeyCode == down2)
+            {
+                Speed.Y = SpeedValue;
+            }
+            if (e.KeyCode == right || e.KeyCode == right2)
+            {
+                Speed.X = SpeedValue;
+            }
+        }
+        public void MoveEnd(KeyEventArgs e, Keys up, Keys down, Keys left, Keys right)
+        {
+            if (e.KeyCode == up || e.KeyCode == down)
             {
                 Speed.Y = 0;
             }
-            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right || e.KeyCode == Keys.A || e.KeyCode == Keys.D)
+            if (e.KeyCode == left || e.KeyCode == right)
+            {
+                Speed.X = 0;
+            }
+        }
+        public void MoveEnd(KeyEventArgs e, Keys up, Keys down, Keys left, Keys right, Keys up2, Keys down2, Keys left2, Keys right2)
+        {
+            if (e.KeyCode == up || e.KeyCode == down || e.KeyCode == up2 || e.KeyCode == down2)
+            {
+                Speed.Y = 0;
+            }
+            if (e.KeyCode == left || e.KeyCode == right || e.KeyCode == left2 || e.KeyCode == right2)
             {
                 Speed.X = 0;
             }
